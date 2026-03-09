8a0045f6c36bfb47318620074779d1e7ba71d341
leomano10 <manologabiraman77@gmail.com>
2026-03-09 14:53:38 +0530

    success

--- mano/mano/settings.py
+++ mano/mano/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-impo
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles','myapp'
 ]
 
 MIDDLEWARE = [
